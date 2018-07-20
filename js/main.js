a412d32092a77d7c866e3de2d650236262d7d43d
James Foss <jamesfoss1@hotmail.com>
2018-07-19 23:01:00 -0400

    Project Notes

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// On page load, cursor appears in the name field.
+
+// Job Role text field appears when users select other from the list.
+
+// Until a theme is selected, the color field for T-shirts will read "Please select a theme."
+// When the T-shirt theme is selected, the color field will limit options to only those avaiable to that theme.
+// Extra Credit: The color field will only appear when a theme is selected.
+
+// Users cannot select activities that are held at the same time.
+// There should be a running total that updates at the bottom of the activities section.
+
+// Credit Card payment option is selected by default.
+// The selected payment option should match what is displayed on screen.
+// When users switch among payment options, the chosen one displays and the others are hidden.
+
+// There should be form validation which interupts the page from refreshing unless met.
+// 1. Name field cannot be blank.
+// 2. Email field should contain a properly formatted email address.
+// 3. At least one checkbox under activities should be selected.
+// 4. If the credit card payment option is selected, the card number should be 13-16 digits, zip 5 digits, & CVV 3 digits.
+
+// On submission, the form provides and error indication or message for each field that requires validation.
+// Extra Credit: The form should provide at least one error message in real time.
+// Extra Credit: The form should provide at least one error message that changes based on the type of error. ie. empty or incorrect.
+
+// When javascript is disabled, all form fields and payment methods should be displayed.
